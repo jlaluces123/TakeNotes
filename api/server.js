640d8ccbe63b6824e779378160eb4fd16bbb25f7
jlaluces123 <jlalucescareer123@gmail.com>
2018-10-29 10:52:56 -0700

    MVP: Created GET endpoint for /api/notes/:id, view an existing note by id

--- api/server.js
+++ api/server.js
@@ -38,5 +38,21 @@ server.get('/api/notes', (req, res) => {
     .catch(err => res.status(500).json({ error: 'Could not retrieve notes' }, err));
 });
 
+server.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('notes').where({ id }).first()
+    .then(note => {
+
+      if (note) {
+        res.status(200).json(note);
+      } else {
+        res.status(404).json({ missingError: 'Could not find a note by that id' });
+      }
+
+    })
+    .catch(err => res.status(500).json({ error: 'An error occurred with the server when making the request, please try again' }, err));
+});
+
 
 module.exports = server;
